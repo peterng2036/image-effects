704cf66a4009d2ae60fadc4eb2d67e643b580343
peterng2036 <kachonng1@gmail.com>
2022-12-18 18:36:54 +0800

    init

---
+++ public/main.js
@@ -0,0 +1,28 @@
+async function init() {
+  let rustApp = null;
+
+  try {
+    rustApp = await import("../pkg");
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+
+  console.log(rustApp);
+  const input = document.getElementById("upload");
+  const fileReader = new FileReader();
+
+  fileReader.onloadend = () => {
+    const base64 = fileReader.result.replace(
+      /^data:image\/(png|jpeg|jpg);base64,/,
+      ""
+    );
+    const imageDataUrl = rustApp.grayscale(base64);
+    document.getElementById("new-img").setAttribute("src", imageDataUrl);
+  };
+  input.addEventListener("change", () => {
+    fileReader.readAsDataURL(input.files[0]);
+  });
+}
+
+init();
